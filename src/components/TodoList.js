7f71bf608cf8f03fd9236506b88386aa22128b3d
Vishal Sharma <785vishal443@gmail.com>
2023-10-27 21:39:56 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,8 +3,15 @@ import { useSelector } from 'react-redux';
 import TodoItem from './TodoItem';
 
 const TodoList = () => {
-  //Write a code as per needed
-  return <ul>{/* // */}</ul>;
+  const todos = useSelector((state) => state.todos);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
